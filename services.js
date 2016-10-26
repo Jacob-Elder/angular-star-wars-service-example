5ef8125855a78af360153e69821926685942ed86
Steve Geluso <stevegeluso@gmail.com>
2016-10-26 15:04:53 -0700

    reducing redundancy in service

--- services.js
+++ services.js
@@ -1,16 +1,14 @@
 app.service('StarWarsService', ['$http', function($http) {
   this.getPlanets = function() {
-    var req = {
-      url: "http://swapi.co/api/planets/",
-      method: "GET",
-      params: {}
-    };
-
-    return $http(req);
+    return apiRequest("planets");
   }
   this.getMovies = function() {
+    return apiRequest("films");
+  }
+
+  function apiRequest(path) {
     var req = {
-      url: "http://swapi.co/api/films/",
+      url: "http://swapi.co/api/" + path,
       method: "GET",
       params: {}
     };
